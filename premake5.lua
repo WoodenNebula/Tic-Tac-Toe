0a0ff7a8a0df5d03e4dbafdd13190bab5538fff0
WoodenNebula <87257970+WoodenNebula@users.noreply.github.com>
2025-02-13 00:50:34 +0545

    feat: add window spawning and color rendering

--- premake5.lua
+++ premake5.lua
@@ -1,18 +1,47 @@
 workspace "Tic-Tac-Toe"
-   configurations { "Debug", "Release" }
-   location "build"
+    configurations { "Debug", "Release" }
+    architecture "x64"
+    location "./Tic-Tac-Toe"
 
-project "Tic-Tac-Toe"
-   kind "ConsoleApp"
-   language "C++"
-   targetdir "bin/%{cfg.buildcfg}"
+    filter "configurations:Debug"
+        defines { "DEBUG", "_DEBUG" }
+        symbols "On"
+    filter{}
 
-   files { "src/**.h", "src/**.cpp" }
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+    filter{}
 
-   filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
+    filter "system:windows"
+        defines { "WINDOWS" }
+    filter{}
 
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
+    filter "system:linux"
+        defines { "LINUX" }
+    filter{}
+
+
+    outputdir = "%{cfg.architecture}-%{cfg.system}-%{cfg.buildcfg}"
+
+    include "Tic-Tac-Toe/Tic-Tac-Toe.lua"
+
+
+newaction {
+    trigger = "clean",
+    description = "Remove all binaries and intermediate binaries, and vs files.",
+    execute = function()
+        print("==== Cleaning Up ====")
+        os.chdir("Tic-Tac-Toe")
+
+        os.rmdir("build")
+        os.rmdir("./.vs")
+
+        os.remove("**.sln")
+        os.remove("**.vcxproj")
+        os.remove("**.vcxproj.**")
+        os.remove("**Makefile")
+        os.remove("**.make")
+        print("==== Success ====\n")
+    end
+}
