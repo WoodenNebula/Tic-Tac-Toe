29aaecc6e2e8434ff275dea9e7f439f3776dce9e
oNebula <87257970+WoodenNebula@users.noreply.github.com>
2025-03-16 17:04:23 +0545

    feat: replace glut code with glfw and glad

--- Tic-Tac-Toe/src/main.cpp
+++ Tic-Tac-Toe/src/main.cpp
@@ -1,23 +1,60 @@
-/*#include "glad/glad.h"*/
-#include <iostream>
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <cstddef>
+#include <iostream>
+
+static void key_callback(GLFWwindow *windowHandle, int key, int scancode,
+                         int action, int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(windowHandle, GLFW_TRUE);
+  }
+}
 
 int main(int argc, char *argv[]) {
-  if (!glfwInit())
-  {
-    std::cout << "[ERROR-GLAD] Initialization failed" << std::endl;
+  // glfw should always be initialized first!!!
+  if (!glfwInit()) {
+    std::cerr << "[ERROR-GLFW] Initialization failed" << std::endl;
+    abort();
   }
 
-
-  /*int x =   gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);*/
+  // set opengl version
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // creating a window
-
   int width = 800, height = 600;
   int x_pos = 0, y_pos = 0;
   const char *windowTitle = "Tic-Tac-Toe";
 
-  // displaying single color
+  GLFWwindow *windowHandle =
+      glfwCreateWindow(width, height, windowTitle, NULL, NULL);
+
+  if (!windowHandle) {
+    std::cerr << "[ERROR-GLFW]  Window Initialization failed" << std::endl;
+    abort();
+  }
+
+  glfwSetKeyCallback(windowHandle, key_callback);
+
+  // this creates a valid window context for opengl to render to
+  glfwMakeContextCurrent(windowHandle);
+
+  // glad should only be initialized after a valid context has been created
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "[ERROR-GLAD] Initialization failed" << std::endl;
+    abort();
+  }
+
+  while (!glfwWindowShouldClose(windowHandle)) {
+    glClearColor(.69, .69, .69, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(windowHandle);
+    glfwPollEvents();
+  }
+
+  // cleanup
+  glfwDestroyWindow(windowHandle);
   glfwTerminate();
   return 0;
 }
