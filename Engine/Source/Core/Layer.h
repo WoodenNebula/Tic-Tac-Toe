a337d0e5a8b5858783428d0c8d5a1605a00a3e0e
Surab Parajuli <87257970+WoodenNebula@users.noreply.github.com>
2025-12-22 09:35:41 +0545

    feat: add layer and layerstack classes

---
+++ Engine/Source/Core/Layer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Events/Event.h"
+#include <string>
+
+namespace Engine
+{
+class Layer
+{
+public:
+    Layer(std::string_view Name = "Layer") : m_Name(Name) {}
+    virtual ~Layer() = default;
+
+    virtual void OnAttach() {}
+    virtual void OnDetach() {}
+    virtual void OnUpdate(float deltaTime) {}
+    virtual void OnEvent(const Events::EventBase& event) {}
+
+    std::string_view GetName() const { return m_Name; }
+protected:
+    std::string m_Name;
+};
+}
